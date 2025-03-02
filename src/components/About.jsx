47d5e219c95af8d2e43c0258acb4524afc1264f9
Addo Raymond <addoraymond123@gmail.com>
2025-03-02 23:56:06 +0000

    making site responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,9 @@ const About = () => {
         <span className="dot"></span></h1>
           <div className='myself'>
 
-            <span className='line'>
+            {/* <span className='line'>
               <hr />
-            </span>
+            </span> */}
             <h1>TLDR</h1>
             <p>I love building cool projects, web development is my primary specialty. All I need is a strong internet connectivity and your work is done. I have acquired a wholesome knowledge in the
             tech field for a very long time and it has made my life and others very easy. I am trustworthy, creative, quick and also an effective communicator. I fondly anticipate to work in a
