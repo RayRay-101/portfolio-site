81a34680b0cc2b149f56d4c26883f8d4b78dc1fa
Addo Raymond <addoraymond123@gmail.com>
2025-03-04 19:53:33 +0000

    C:/Program Files/Git/

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Ensure this is an empty array
+    }
+  },
+  optimizeDeps: {
+    include: ['@emailjs/browser']
+  }
 })
